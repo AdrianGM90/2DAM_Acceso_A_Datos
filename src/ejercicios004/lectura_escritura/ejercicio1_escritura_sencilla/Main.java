32a5cacba8157dedc3d6ac328f9f8b1a3f621940
Adrian Guerrero <adrian.guerrero.martinez.1990@gmail.com>
2025-10-01 21:56:32 +0200

    UPDATE: Se añaden los ejercicios 004_1 y 004_2

---
+++ src/ejercicios004/lectura_escritura/ejercicio1_escritura_sencilla/Main.java
@@ -0,0 +1,28 @@
+package ejercicios004.lectura_escritura.ejercicio1_escritura_sencilla;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File resources = new File("src/ejercicios004/lectura_escritura/ejercicio1_escritura_sencilla/resources/");
+        if (!resources.exists()) {
+            resources.mkdirs();
+        }
+
+        String fileName = "src/ejercicios004/lectura_escritura/ejercicio1_escritura_sencilla/resources/paises.txt";
+
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+            String[] paises = {"Spain", "UK", "Portugal", "France", "Germany" };
+            for (String pais : paises) {
+                bw.write(pais + "\n");
+            }
+            System.out.println("Escritura finalizada.");
+        } catch (IOException e) {
+            System.err.println("Error en la escritura del fichero: " + e.getMessage());
+        }
+    }
+}
---
+++ src/ejercicios004/lectura_escritura/ejercicio2_lectura_sencilla/Main.java
@@ -0,0 +1,21 @@
+package ejercicios004.lectura_escritura.ejercicio2_lectura_sencilla;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String fileName = "src/ejercicios004/lectura_escritura/ejercicio1_escritura_sencilla/resources/paises.txt";
+
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            String line;
+            while((line = br.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            System.err.println("Error en la lectura del archivo: " + e.getMessage());
+        }
+    }
+}
